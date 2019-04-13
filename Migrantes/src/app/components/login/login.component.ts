de6fc9d36662e4134fa6108e46581b05c703ea57
Ludagon <37935555+Ludagon@users.noreply.github.com>
2019-04-13 17:26:24 -0400

    Login y registro

--- Migrantes/src/app/components/login/login.component.ts
+++ Migrantes/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+
+  hide = true;
   constructor() { }
 
   ngOnInit() {
   }
+  submit(){
+    //Auth
+  }
+  register(){
 
+  }
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'debe ingresar un valor' :
+        this.email.hasError('email') ? 'este correo no es valido' :
+        '';
+  }
+  getErrorPassword(){
+    return this.password.hasError('required') ? 'debe ingresar un valor':
+    this.password.hasError('minlength') ? 'Debe tener al menos 8 caracteres' :
+    '';
+    
+  }
 }
