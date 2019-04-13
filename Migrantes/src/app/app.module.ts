d32e4f94b10c3619b86fe41ee9e113fa85f3a088
Ludagon <37935555+Ludagon@users.noreply.github.com>
2019-04-13 14:08:32 -0400

    Navbar(generico) - Modulos de material

--- Migrantes/src/app/app.module.ts
+++ Migrantes/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
+
+//Material Modules
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatIconModule, MatCardModule , } from '@angular/material';
+import {MatToolbarModule} from '@angular/material'
+import {MatTabsModule, matTabsAnimations} from '@angular/material/tabs'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list'
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule, 
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatProgressBarModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
